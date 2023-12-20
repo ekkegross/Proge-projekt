2a95eefe477fc37b58d2e3abb9515ee8298eca73
ReimoK <reimokaabel@gmail.com>
2023-12-20 18:22:24 +0200

    hud ja unitid

--- mäng.py
+++ mäng.py
@@ -66,7 +66,54 @@ class Unit(pygame.sprite.Sprite):
             screen.blit(text, (WIDTH // 1 - text.get_width() // 1, HEIGHT // 2 - text.get_height() // 2))
             pygame.display.flip()
             pygame.time.wait(2000)  
-            return "green"  
+            return "green"
+class Tank(pygame.sprite.Sprite):
+    def __init__(self, color, x, y, target_base, target_units, shared_base_health):
+        super().__init__()
+        self.image = pygame.image.load("tank.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed = 1.5
+        self.target_base = target_base
+        self.target_units = target_units
+        self.health = 100
+        self.shared_base_health = shared_base_health
+
+    def update(self):
+        if self.health <= 0:
+            # Kui üksus on hävitatud eemalda grupp
+            self.kill()
+
+        target_x, target_y = self.target_base
+        distance_to_base = pygame.math.Vector2(target_x - self.rect.x, target_y - self.rect.y)
+
+        # Liigu baasi poole
+        distance_to_base_length = distance_to_base.length()
+        if distance_to_base_length > 0:
+            distance_to_base.normalize_ip()
+            self.rect.x += distance_to_base.x * self.speed
+            self.rect.y += distance_to_base.y * self.speed
+
+        # KOntrolli kas läheduses on üksusi
+        for other_unit in self.target_units:
+            if other_unit != self and pygame.sprite.collide_rect(self, other_unit):
+                # Ründa üksust
+                other_unit.health -= 1
+
+        # Kontrolli kas üksused on punase baasi läheduses, kui on võta elusi vähemaks
+        if distance_to_base_length < 50:
+            self.shared_base_health[0] -= 2
+            pygame.time.wait(100)
+            print(f"Red base health: {self.shared_base_health[0]}")# Näita baasi elusid
+
+        # Kontrolli kas baas on hävitatud
+        if self.shared_base_health[0] <= 0:
+            font = pygame.font.Font(None, 74)
+            text = font.render("Green team! Press SPACE to restart, ESC to exit.!", True, BLUE)
+            screen.blit(text, (WIDTH // 1 - text.get_width() // 1, HEIGHT // 2 - text.get_height() // 2))
+            pygame.display.flip()
+            pygame.time.wait(2000)  
+            return "green"
 
 # Punase üksuste klass, sama mis enne lihtsalt teisel pool
 class Unit1(pygame.sprite.Sprite):
@@ -111,7 +158,48 @@ class Unit1(pygame.sprite.Sprite):
             pygame.display.flip()
             pygame.time.wait(2000)  
             return "red"  
+class Tank1(pygame.sprite.Sprite):
+    def __init__(self, color, x, y, target_base, target_units, shared_base_health):
+        super().__init__()
+        self.image = pygame.image.load("tank1.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.speed = 1.5
+        self.target_base = target_base
+        self.target_units = target_units
+        self.health = 100
+        self.shared_base_health = shared_base_health
 
+    def update(self):
+        if self.health <= 0:
+            self.kill()
+
+        target_x, target_y = self.target_base
+        distance_to_base = pygame.math.Vector2(target_x - self.rect.x, target_y - self.rect.y)
+
+        distance_to_base_length = distance_to_base.length()
+        if distance_to_base_length > 0:
+            distance_to_base.normalize_ip()
+            self.rect.x += distance_to_base.x * self.speed
+            self.rect.y += distance_to_base.y * self.speed
+
+        for other_unit in self.target_units:
+            if other_unit != self and pygame.sprite.collide_rect(self, other_unit):
+                # Ründa teist üksust
+                other_unit.health -= 1
+
+        if distance_to_base_length < 50:            
+            self.shared_base_health[0] -= 2
+            pygame.time.wait(500)
+            print(f"Green base health: {self.shared_base_health[0]}")
+
+        if self.shared_base_health[0] <= 0:
+            font = pygame.font.Font(None, 74)
+            text = font.render("Red team won! Press SPACE to restart, ESC to exit.", True, BLUE)
+            screen.blit(text, (WIDTH // 3 - text.get_width() // 3, HEIGHT // 2 - text.get_height() // 2))
+            pygame.display.flip()
+            pygame.time.wait(2000)  
+            return "red"  
 # Turreti class
 class Turret(pygame.sprite.Sprite):
     def __init__(self, color, x, y, target_units):
@@ -120,7 +208,7 @@ class Turret(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.target_units = target_units
-        self.attack_range = 100
+        self.attack_range = 750
         self.attack_cooldown = 0
 
     def update(self):
@@ -132,7 +220,7 @@ class Turret(pygame.sprite.Sprite):
             if pygame.sprite.collide_rect(self, other_unit) and self.attack_cooldown == 0:
                 # Vähenda vaenlase üksuse elusi
                 other_unit.health -= 5
-                self.attack_cooldown = FPS  # Ooteaeg turreti laskmiste vahel
+                self.attack_cooldown = 10  # Ooteaeg turreti laskmiste vahel
 
 class Turret1(pygame.sprite.Sprite):
     def __init__(self, color, x, y, target_units):
@@ -141,7 +229,7 @@ class Turret1(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.target_units = target_units
-        self.attack_range = 100
+        self.attack_range = 750
         self.attack_cooldown = 0
 
     def update(self):
@@ -153,8 +241,50 @@ class Turret1(pygame.sprite.Sprite):
             if pygame.sprite.collide_rect(self, other_unit) and self.attack_cooldown == 0:
                 # Vähenda vaenlase üksuse elusi
                 other_unit.health -= 5
-                self.attack_cooldown = FPS  # Ooteaeg turreti laskmiste vahel
+                self.attack_cooldown = 10  # Ooteaeg turreti laskmiste vahel
+                
+class Turret2(pygame.sprite.Sprite):
+    def __init__(self, color, x, y, target_units):
+        super().__init__()
+        self.image = pygame.image.load("turret2.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.target_units = target_units
+        self.attack_range = 1000
+        self.attack_cooldown = 0
 
+    def update(self):
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+
+        # Vaata kas läheduses on vaenlase üksusi
+        for other_unit in self.target_units:
+            if pygame.sprite.collide_rect(self, other_unit) and self.attack_cooldown == 0:
+                # Vähenda vaenlase üksuse elusi
+                other_unit.health -= 10
+                self.attack_cooldown = 10  # Ooteaeg turreti laskmiste vahel
+                
+class Turret3(pygame.sprite.Sprite):
+    def __init__(self, color, x, y, target_units):
+        super().__init__()
+        self.image = pygame.image.load("turret3.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.target_units = target_units
+        self.attack_range = 1000
+        self.attack_cooldown = 0
+
+    def update(self):
+        if self.attack_cooldown > 0:
+            self.attack_cooldown -= 1
+
+        # Vaata kas läheduses on vaenlase üksusi
+        for other_unit in self.target_units:
+            if pygame.sprite.collide_rect(self, other_unit) and self.attack_cooldown == 0:
+                # Vähenda vaenlase üksuse elusi
+                other_unit.health -= 10
+                self.attack_cooldown = 10  # Ooteaeg turreti laskmiste vahel
+                
 # Rohelise üksuste grupp
 player_units_group = pygame.sprite.Group()
 
@@ -175,7 +305,8 @@ shared_red_base_health = [100]
 shared_green_base_health = [100]
 rohelinebaas = pygame.image.load('baas.png')
 punanebaas = pygame.image.load('baas1.png')
-
+rohelinehud = pygame.image.load('hud.png')
+punanehud = pygame.image.load('hud1.png')
 # Mängu seis
 game_over = False
 
@@ -205,26 +336,51 @@ while running:
             running = False
 
         if not game_over:
-            # Klahv 'E' vajutamisel saada roheline üksus välja
-            if keys[pygame.K_e]:
+            # Rohelise baasi tegelased
+            
+            # Klahv 'Q' vajutamisel saada roheline üksus välja
+            if keys[pygame.K_q]:
                 player_unit = Unit(GREEN, 50, HEIGHT // 2, (WIDTH - 100, HEIGHT // 2), enemy_units_group, shared_red_base_health)
                 player_units_group.add(player_unit)
-
-            # Klahv 'P' vajutamisel saada punane üksus välja
-            if keys[pygame.K_p]:
-                enemy_unit = Unit1(RED, WIDTH - 50, HEIGHT // 2, (50, HEIGHT // 2), player_units_group, shared_green_base_health)
-                enemy_units_group.add(enemy_unit)
-
-            # Klahv 'T' vajutamisel pane roheline turret maha
-            if keys[pygame.K_t]:
+            
+            # Klahv 'W' vajutamisel saada roheline üksus välja
+            if keys[pygame.K_w]:
+                player_unit = Tank(GREEN, 50, HEIGHT // 2, (WIDTH - 100, HEIGHT // 2), enemy_units_group, shared_red_base_health)
+                player_units_group.add(player_unit)
+                
+            # Klahv 'E' vajutamisel pane roheline turret maha
+            if keys[pygame.K_e]:
                 if len(player_turrets_group) < MAX_TURRETS:
                     player_turret = Turret(GREEN, 120, HEIGHT // 2, enemy_units_group)
                     player_turrets_group.add(player_turret)
-
-            # Klahv 'Y' vajutamisel pane punane turret maha
+            # Klahv R' vajutamisel pane tugevam roheline turret maha
+            if keys[pygame.K_r]:
+                if len(player_turrets_group) < MAX_TURRETS:
+                    player_turret = Turret2(GREEN, 120, HEIGHT // 2, enemy_units_group)
+                    player_turrets_group.add(player_turret)  
+            
+            # Punase baasi tegelased
+            
+            # Klahv 'Y' vajutamisel saada punane üksus välja
             if keys[pygame.K_y]:
+                enemy_unit = Unit1(RED, WIDTH - 50, HEIGHT // 2, (50, HEIGHT // 2), player_units_group, shared_green_base_health)
+                enemy_units_group.add(enemy_unit)
+                
+            # Klahv 'U' vajutamisel saada punane üksus välja
+            if keys[pygame.K_u]:
+                enemy_unit = Tank1(RED, WIDTH - 50, HEIGHT // 2, (50, HEIGHT // 2), player_units_group, shared_green_base_health)
+                enemy_units_group.add(enemy_unit)
+
+            # Klahv 'I' vajutamisel pane punane turret maha
+            if keys[pygame.K_i]:
                 if len(enemy_turrets_group) < MAX_TURRETS:
                     enemy_turret = Turret1(RED, WIDTH - 125, HEIGHT // 2, player_units_group)
+                    enemy_turrets_group.add(enemy_turret)
+                    
+            # Klahv 'O' vajutamisel pane tugevam punane turret maha
+            if keys[pygame.K_o]:
+                if len(enemy_turrets_group) < MAX_TURRETS:
+                    enemy_turret = Turret3(RED, WIDTH - 125, HEIGHT // 2, player_units_group)
                     enemy_turrets_group.add(enemy_turret)
 
     if not game_over:
@@ -259,10 +415,14 @@ while running:
             enemy_units_group.empty()
             player_turrets_group.empty()
             enemy_turrets_group.empty()
-    # Taust
+    # Taust 
     taust = pygame.image.load("Taust.png")
     screen.blit(taust, (0, 0))
-
+    
+    # HUD
+    screen.blit(rohelinehud, (0, HEIGHT // 2 - 500))
+    screen.blit(punanehud, (WIDTH - 250, HEIGHT // 2 - 500))
+    
     # Rohelise baasi ruut
     screen.blit(rohelinebaas, (20, HEIGHT // 2 - 40))
     # Rohelise baasi elud
